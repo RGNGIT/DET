a7ea1b58ae509ec3528d8d74f5e9767539f743a5
RGNGIT <68756004+RGNGIT@users.noreply.github.com>
2021-03-06 02:22:40 +0500

    Add files via upload

---
+++ _Det/_Det/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _Det
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStep = new System.Windows.Forms.Button();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            this.buttonCompute = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStep
+            // 
+            this.buttonStep.Location = new System.Drawing.Point(911, 39);
+            this.buttonStep.Name = "buttonStep";
+            this.buttonStep.Size = new System.Drawing.Size(125, 23);
+            this.buttonStep.TabIndex = 0;
+            this.buttonStep.Text = "Создать";
+            this.buttonStep.UseVisualStyleBackColor = true;
+            this.buttonStep.Click += new System.EventHandler(this.buttonStep_Click);
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Location = new System.Drawing.Point(911, 12);
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.Size = new System.Drawing.Size(125, 20);
+            this.textBoxSize.TabIndex = 3;
+            // 
+            // buttonCompute
+            // 
+            this.buttonCompute.Location = new System.Drawing.Point(911, 9);
+            this.buttonCompute.Name = "buttonCompute";
+            this.buttonCompute.Size = new System.Drawing.Size(125, 23);
+            this.buttonCompute.TabIndex = 4;
+            this.buttonCompute.Text = "Вычислить";
+            this.buttonCompute.UseVisualStyleBackColor = true;
+            this.buttonCompute.Click += new System.EventHandler(this.buttonCompute_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(12, 474);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(68, 13);
+            this.labelInfo.TabIndex = 5;
+            this.labelInfo.Text = "Ожидание...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 496);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonCompute);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.buttonStep);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStep;
+        private System.Windows.Forms.TextBox textBoxSize;
+        private System.Windows.Forms.Button buttonCompute;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ _Det/_Det/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace _Det {
+
+    public partial class Form1 : Form {
+
+        List<string> LogList = new List<string>(); // Список шагов для вывода в файл
+
+        public Form1() { // Первичная инициализация программы
+            InitializeComponent();
+            buttonCompute.Visible = false;
+        }
+
+        async void Gauss(int mSize) { // Асинхронный метод Гаусса для вычисления определителя
+            double[,] Array = new double[mSize, mSize]; // Создаем локальный двухмерный массив для записи в него нашей матрицы
+
+            for (int i = 0; i < mSize; i++) { // Цикл записи данных в матрицу
+                for (int j = 0; j < mSize; j++) {
+                    Array[i, j] = Convert.ToDouble(Controls[$"input{i}{j}"].Text); // Записываем поэлементно данные из окошек матрицы
+                }
+            }
+
+            double buff = 0, times = 1; // Объявление локальный переменных для алгоритма Гаусса
+
+            for (int i = 0; i < mSize - 1; i++) { // Снова обходим матрицу
+                for (int j = i + 1; j < mSize; j++) {
+                    if (Array[i, i] != 0) { // Элемент главной диагонали == 0
+                        buff = Array[j, i] / Array[i, i]; // Закидываем в буффер частное элемента на 1 дальше и текущего элемента главной диагонали
+                    }
+                    for (int k = i; k < mSize; k++) { // Цикл работы с текущим столбцом
+                        labelInfo.Text = $"Вычитаем из элемента {j}го столбца {k}й строки ({Array[j, k]}) произведения э-та {i}го столбца {k}й строки ({Array[i, k] * buff}) на {buff} (частное {j}го столбца {i}й строки и {i}го столбца {i}й строки)";
+                        LogList.Add(labelInfo.Text); // Сверху вывод лога на экран + запись каждого шага в список для вывода в файл
+                        Array[j, k] -= Math.Floor((Array[i, k] * buff) * 100) / 100; // Вычитание с округлением (Floor - пол -- нижняя граница дробного числа)
+                        await Task.Delay(TimeSpan.FromMilliseconds(1000)); // Задержка в 1 секунду
+                        for (int i1 = 0; i1 < mSize; i1++) { // Цикл обхода матрицы для пошагового вывода на экран
+                            for (int j1 = 0; j1 < mSize; j1++) {
+                                Controls[$"input{i1}{j1}"].Text = Array[i1, j1].ToString(); // Запись значений в окошки матрицы
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < mSize; i++) {
+                times *= Array[i, i]; // После алгоритма перемножаем элементы главной диагонали
+            }
+
+            labelInfo.Text = $"Детерминант матрицы: {times}"; // Выводим детерминант на экран
+
+            int f = 0; string[] ToFile = new string[LogList.Count]; // Запись в файл. Создаем новый строчный массив размера получившегося списка
+            foreach (string capture in LogList) { // Проходимся по списку и в каждый элемент строчного массива забиваем данные из списка
+                ToFile[f] = $"Шаг {f + 1} :: {capture}"; f++;
+            }
+
+            File.WriteAllLines("Log.txt", ToFile); // Записываем в файл (файл спавнится рядом с .exe)
+
+        }
+
+        private void buttonStep_Click(object sender, EventArgs e) { // Обработчик нажатия кнопки "создать"
+            textBoxSize.Visible = false; // Делаем невидимым окно ввода размера матрицы
+            buttonStep.Visible = false; // Делаем невидимым кнопку "создать"
+            buttonCompute.Visible = true; // Кнопку "вычислить" делаем видимой
+            int xStart = 12, yStart = 12; // Позиция окошка матрицы 0 строки 0 столбца (верхний левый угол) от которого строим остальные окна
+            for (int i = 0; i < Convert.ToInt32(textBoxSize.Text); i++) { // Цикл обхода по матрице
+                for (int j = 0; j < Convert.ToInt32(textBoxSize.Text); j++) {
+                    TextBox Box = new TextBox(); // Создаем новый экземпляр окошка матрицы
+                    Box.Name = $"input{i}{j}"; // Ставим ему имя "input" + цифры его координат в матрице
+                    Box.Size = new Size(25, 20); // Ставим размер окна
+                    Box.Location = new Point(xStart + (i * 33), yStart + (j * 26)); // Вычисление позиций окошек матрицы. Каждое окно дальше на 33 вправо и ниже на 26 вниз
+                    Controls.Add(Box); // Добавляем на окно программы получившийся объект
+                }
+            }
+        }
+
+        private void buttonCompute_Click(object sender, EventArgs e) { // Обработчик нажатия кнопки вычислить
+            buttonCompute.Visible = false; // Делаем невидимой кнопку "вычислить"
+            Gauss(Convert.ToInt32(textBoxSize.Text)); // Вызываем функцию вычисления, отправляя параметром порядок матрицы
+        }
+
+    }
+}
